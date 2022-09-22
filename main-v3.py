2d84a7d5a8ce8c49252f440403b36ae805a24648
XingyuLi20 <lixingyu@berkeley.edu>
2022-09-22 10:57:51 -0700

    add v3

---
+++ main-v3.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+from joblib import Parallel, delayed
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--log-level=3")
+options.add_extension("uBlock-Origin.crx")
+
+# uncommect below line to use headless mode
+# options.headless = True
+
+
+def main():
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
+    driver.get("https://www.toutoupiao.com/VoteItem/428720")
+    while True:
+        driver.find_element("id", "428720").click()
+        while driver.find_element("id", "428720").text != "VOTED":
+            sleep(0.5)
+        driver.delete_all_cookies()
+        driver.refresh()
+
+
+Parallel(n_jobs=-1)(delayed(main)() for _ in range(20))
